c571b8b6cb9886129e626caf4085c90c0a16ecd1
kbbansi <kwabenaampofo5@gmail.com>
2021-01-08 10:13:54 +0000

    Add login and sign-up components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, SignUpComponent, LoginComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
