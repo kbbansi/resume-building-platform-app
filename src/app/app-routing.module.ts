85aebe1fd4ca37d9acf07712b302aa8cc48212c4
kbbansi <kwabenaampofo5@gmail.com>
2021-01-09 05:53:10 +0000

    Added Email Login Screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'application', loadChildren: () => import('./application/application.module').then(m => m.ApplicationModule) }
+
+  //::: todo
+  // investigate the use of redirect routes and protected routes:
 ];
 
 @NgModule({
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent }
 
 ];
 
