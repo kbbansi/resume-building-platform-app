a48014db2e8037ed6edefde8766ab6fc791a256b
kbbansi <kwabenaampofo5@gmail.com>
2021-01-23 21:10:05 +0000

    Add live api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // api url
-  resume_url: 'http://localhost:3000/'
+  resume_url: 'https://resume-building-platform-api.herokuapp.com/' //testing: 'http://localhost:3000/'
 };
